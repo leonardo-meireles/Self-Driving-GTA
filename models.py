b133f6be8fd324fb0f8764bc5146eafe69a042c2
leoMurtha <aka.murtha@gmail.com>
2018-11-08 21:21:50 -0200

    changes to models and train load file

--- models.py
+++ models.py
@@ -14,62 +14,8 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import local_response_normalization
 
-def alexnet(width, height, lr):
-    # Start Layers
-    
-    network = input_data(shape=[None, width, height, 1], name='input')
-    
-    network = conv_2d(network, 96, 11, strides=4, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    
-    network = conv_2d(network, 256, 5, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
-    
-    network = conv_2d(network, 256, 3, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    
-    network = conv_2d(network, 256, 5, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
-    
-    network = conv_2d(network, 384, 3, activation='relu')
-    network = conv_2d(network, 384, 3, activation='relu')
-   
-    network = conv_2d(network, 256, 3, activation='relu')
-    network = max_pool_2d(network, 3, strides=2)
-    network = local_response_normalization(network)
 
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    
-    network = fully_connected(network, 4096, activation='tanh')
-    network = dropout(network, 0.5)
-    
-    network = fully_connected(network, 3, activation='softmax')
-    #End Layers
-
-
-    network = regression(network, optimizer='momentum',
-                         loss='categorical_crossentropy',
-                         learning_rate=lr, name='targets')
-
-    model = tflearn.DNN(network, tensorboard_verbose=2, tensorboard_dir='trained_models/log')
-
-    return model
-
-
-def alexnetMod(width, height, lr):
+def alexnet(width, height, lr, batch=64):
     # Start Layers
 
     network = input_data(shape=[None, width, height, 1], name='input')
@@ -114,7 +60,62 @@ def alexnetMod(width, height, lr):
     network = fully_connected(network, 3, activation='softmax')
     # End Layers
 
-    network = regression(network, optimizer='adam', batch_size=128,
+    network = regression(network, optimizer='momentum',
+                         loss='categorical_crossentropy',
+                         learning_rate=lr, name='targets')
+
+    model = tflearn.DNN(network, tensorboard_verbose=2, batch_size=batch,
+                        tensorboard_dir='trained_models/log')
+
+    return model
+
+
+def alexnetAdam(width, height, lr, batch):
+    # Start Layers
+
+    network = input_data(shape=[None, width, height, 1], name='input')
+
+    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+
+    network = fully_connected(network, 3, activation='softmax')
+    # End Layers
+
+    network = regression(network, optimizer='adam', batch_size=batch,
                          loss='categorical_crossentropy',
                          learning_rate=lr, name='targets')
 
--- train.py
+++ train.py
@@ -1,37 +1,42 @@
 import numpy as np
-from models.alexnet import alexnet
+from models import alexnetAdam
 import sys
 from utils import load_data
 
 WIDTH = 160
 HEIGHT = 120
 LR = 1e-3
-EPOCHS = 15
+EPOCHS = 1
+BATCH_SIZE = 128
 
 
-def main(id):
-    model = alexnet(WIDTH, HEIGHT, LR)
-    train_data = load_data('data/balanced/balanced_data_%s.npy')
-        
+def main(id, n_files):
+    n_files = int(n_files)
+    model = alexnetAdam(WIDTH, HEIGHT, LR, BATCH_SIZE)
+    
     for i in range(EPOCHS):
-        
-        size_test = int(0.1*len(train_data)) 
-        print('TEST SIZE : %s' % size_test)
-        train = train_data[:-size_test]
-        test = train_data[-size_test:]
+        for j in range(n_files):
+            train_data = np.load('data/balanced/balanced_data_%s.npy' % j)
+            print('LOADED balanced_data_ % s.npy' % j)
 
-        X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
-        Y = [i[1] for i in train]
+            size_test = int(0.1*len(train_data))
+            train = train_data[:-size_test]
+            test = train_data[-size_test:]
 
-        test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
-        test_y = [i[1] for i in test]
+            X = np.array([i[0] for i in train]).reshape(-1, WIDTH, HEIGHT, 1)
+            Y = [i[1] for i in train]
 
-        model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
-            snapshot_step=500, show_metric=True, run_id='gta_%s_%s_%s' % ('alexnet',EPOCHS, id))
-        
-        model.save('trained_models/model.tflearn')
+            test_x = np.array([i[0]
+                               for i in test]).reshape(-1, WIDTH, HEIGHT, 1)
+            test_y = [i[1] for i in test]
+
+            model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}),
+                      snapshot_step=500, show_metric=True, run_id='model_%s_%s_%s' % ('alexnetAdam', EPOCHS, id))
+
+            model.save('trained_models/model.tflearn')
 
         # tensorboard --logdir=foo:C:/path/to/log
 
+
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
